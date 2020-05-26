fa5b4498eaae3465e7e083919fea895740a36642
liemb <lb6031@bs.amalnet.k12.il>
2020-05-26 09:42:37 +0300

    end design

--- app/src/main/java/com/example/ex062/MainActivity.java
+++ app/src/main/java/com/example/ex062/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.ex062;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout ll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ll = (LinearLayout) findViewById(R.id.activity_main);
     }
 }
