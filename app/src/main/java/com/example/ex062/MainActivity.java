3c11038c2fdb342c85c89429ec48c55e9951a61a
av5350@bs.amalnet.k12.il <itayw5350>
2020-05-26 18:46:15 +0300

    start code

--- app/src/main/java/com/example/ex062/MainActivity.java
+++ app/src/main/java/com/example/ex062/MainActivity.java
@@ -3,11 +3,26 @@ package com.example.ex062;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
     LinearLayout ll;
+
+    Switch swB;
+
+    RadioGroup rg;
+
+    RadioButton rbRed, rbGreen, rbBlue, rbYellow;
+
+    Boolean rbCheck;
+
+    RadioButton[] idArr;
+    String[] valuesArr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +30,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ll = (LinearLayout) findViewById(R.id.activity_main);
+
+        swB = (Switch) findViewById(R.id.swB);
+
+        rg = (RadioGroup) findViewById(R.id.rg);
+
+        rbRed = (RadioButton) findViewById(R.id.rbRed);
+        rbGreen = (RadioButton) findViewById(R.id.rbGreen);
+        rbBlue = (RadioButton) findViewById(R.id.rbBlue);
+        rbYellow = (RadioButton) findViewById(R.id.rbYellow);
+
+        rbCheck = false;
+
+        valuesArr = new String[]{"red", "green", "blue", "yellow"};
+        idArr = new RadioButton[]{rbRed, rbGreen, rbBlue, rbYellow};
+        // TODO: add the thing of change bc && check if idArr is needed!!!
+
+    }
+
+    public void rgClicked(View view) {
+        rbCheck = true;
+    }
+
+    public void changeBC(View view) {
+        if (rbCheck)
+        {
+            int rbID = rg.getCheckedRadioButtonId();
+        }
     }
 }
